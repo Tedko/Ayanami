ca6bc4efa9c08106547dee371ea112f41d10302a
Han <yanhan02008@gmail.com>
2015-07-17 08:40:01 +0800

    gitign

--- processData.sh
+++ processData.sh
@@ -6,17 +6,21 @@ echo 'Deleting all URLs...'
 tr ' ' '\n'< output_trainingData_noURL.txt > output_trainingData_lined.txt
 sed '/^$/d' output_trainingData_lined.txt > output_trainingData_pure.txt
 echo 'Purifing Data...'
-rm output_trainingData.txt 
+#rm output_trainingData.txt 
 rm output_trainingData_noURL.txt
 rm output_trainingData_lined.txt 
 mv  output_trainingData_pure.txt input.txt
 echo 'Run node go.js for Chinese Porter Stemmer...'
 node go.js
-mv output.txt output_removeBlankLine.txt
-sed '/^$/d' output_removeBlankLine.txt > output_pure.txt
-rm output_removeBlankLine.txt
-sed -n '/error/d' output_pure.txt
-mv output_pure.txt output.txt
-echo 'result is in the output.txt , Thx! :)'
+sed '/^error/d' output.txt >output_pure.txt
+sed '/^$/d' output_pure.txt > output_removeBlankLine.txt
+rm output_pure.txt
+mv output_removeBlankLine.txt output_ProcessSameLine.txt
+sort output_ProcessSameLine.txt|uniq -c  > output_withSameLineNumber.txt
+sort output_withSameLineNumber.txt > output_final.txt
+rm output_withSameLineNumber.txt
+
+echo ' Chinese Porter Stemmer result is in the output_final.txt'
+
 
 
