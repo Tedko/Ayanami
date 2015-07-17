04c5d5e2a48cae839f4dd854b12c99b10e3c7b2c
plantvsbirds <plantvsbird@gmail.com>
2015-07-17 14:50:01 +0800

    aaa

--- sentiment.js
+++ sentiment.js
@@ -15,7 +15,29 @@ function warn(something) {
 //__inspect(snlp)
 
 
+var pty = require('pty.js');
+module.exports = {
+	test : function (test,cb) {
+		var term = pty.spawn('python /Users/henry/2code/sentimental/sentimental/sentiments.py', [], {
+		  name: 'xterm-color',
+		  cols: 80,
+		  rows: 30,
+		  cwd: process.env.HOME,
+		  env: process.env
+		});
 
+		term.on('data', function(data) {
+		  cb(data);
+		});
+		setTimeout ( function (){
+			term.write(JSON.stringify(['123',test]));
+		}, 2000);
+		
+	}
+}
+
+
+/*
 var cpSpawn = require('child_process').spawn
 	module.exports = {
 		test : function (test,cb) {
@@ -33,4 +55,4 @@ var cpSpawn = require('child_process').spawn
 			});
 
 		}
-}
+}*/
